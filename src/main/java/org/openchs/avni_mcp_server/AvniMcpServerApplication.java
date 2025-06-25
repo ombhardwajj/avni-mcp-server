20cc11ea51a4c3a3efdccac975dc3077cd7edde6
Om Bhardwaj <115864495+ombhardwajj@users.noreply.github.com>
2025-06-25 07:35:03 +0530

    Initialize the project

---
+++ src/main/java/org/openchs/avni_mcp_server/AvniMcpServerApplication.java
@@ -0,0 +1,13 @@
+package org.openchs.avni_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvniMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvniMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/openchs/avni_mcp_server/AvniMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openchs.avni_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvniMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
